8fccaa11a6829d45e48b9004265d02184a2b5f5b
JustinBench <d00188916@utahtech.edu>
2026-09-22 13:11:20 -0600

    need-to-figure-out-css

--- server.py
+++ server.py
@@ -3,5 +3,14 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def main():
+    return "<p>Main page</p>"
+
+
+@app.route("/zelda")
+def zelda_quiz():
+    url = "../Random Generation/index.html"
+    file = open(url, "r")
+    zelda_html = file.read()
+    file.close()
+    return zelda_html
